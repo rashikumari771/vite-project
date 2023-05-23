2f8077a4bd7f98748828789e3be706b9e4a9e3b2
Rashi Kumari <rashikumari617@gamil.com>
2023-05-24 00:23:17 +0530

    //first commit//

---
+++ index.js
@@ -0,0 +1,14 @@
+// let person = {
+//   name: 'mosh',
+//   age:30
+// };
+// person.name='rashi'
+// console.log(person.name);
+
+
+
+function square(number) {
+    return number * number;
+}
+  ading new think
+  console.log(square(2));
